b65c50573c486582079f82bd88eeaea92b4d0ae2
isa kadri <isakadri639@gmail.com>
2026-06-20 14:36:36 +0530

    Add unique elements finder using HashSet
    Implement a program to find unique elements in an array using HashSet.

--- Hasset_problem.java
+++ Hasset_problem.java
@@ -1,1 +1,9 @@
-
+// find unique elements in array
+class Hashset_problem{
+  public static void main(String [] args){
+    int arr[]={1,2,3,4,5,6};
+    HashSet<Integer>set=new HashSet();
+    for(int e :arr){set.add(e);}
+    System.out.println(set.size());
+    
+  }
