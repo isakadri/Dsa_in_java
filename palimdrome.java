6f233954398f8d848a82c040bfb5be4f39b00df5
isa kadri <isakadri639@gmail.com>
2026-06-22 08:07:32 +0530

    Add palindrome checker class and main method

---
+++ palimdrome.java
@@ -0,0 +1,29 @@
+class palimdrome{
+  public boolean check(String str){
+    
+    int low=0;
+    int  high= str.length()-1;
+    while(low<high){
+      if(str.charAt(low)!=str.charAt(high)){
+        return false;
+      }
+        
+      
+      low++;
+      high--;
+      
+        
+    }
+    return true;
+  }
+  public static void main (String []args){
+    String str="III";
+    palimdrome p=new palimdrome();
+    boolean result=p.check(str);
+    if(result){
+      System.out.println("yes its a pamimdrome");
+    }else{
+      System.out.println("no not a palimdrome");
+    }
+  }
+}
